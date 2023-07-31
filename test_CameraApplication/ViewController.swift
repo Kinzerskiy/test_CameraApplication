9ca5eb3f658a715ab09692ee2aa178554056f666
Kinzerskiy <kinzzersky@gmail.com>
2023-07-31 13:17:47 +0300

    Initial Commit

---
+++ test_CameraApplication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test_CameraApplication
+//
+//  Created by Mac Pro on 31.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
